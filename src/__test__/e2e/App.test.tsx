e65eddfe71bb8d72c6840ed179556a10e33c2592
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-04-12 14:24:54 -0300

    tests: define the specs

--- src/__test__/e2e/App.test.tsx
+++ src/__test__/e2e/App.test.tsx
@@ -1,5 +1,23 @@
-describe('Description', () => {
-  test('My test', () => {
+describe('main page tests', () => {
+  it('needs to show all the components, except the filter ones, on screen', () => {
+    expect(true).toBe(true);
+  });
+  it('needs to contain a main random user card from api, with correct collapsed data', () => {
+    expect(true).toBe(true);
+  });
+  it('needs to contain the rest of the list (page) of random users, only with the avatar and the name', () => {
+    expect(true).toBe(true);
+  });
+  it('needs, from the expanded card of selected random user, the detailed info', () => {
+    expect(true).toBe(true);
+  });
+  it('needs a local storage for the colected data', () => {
+    expect(true).toBe(true);
+  });
+  it('needs to recover data from local storage, if the api fails, with a warning', () => {
+    expect(true).toBe(true);
+  });
+  it('needs, as the api developer page, a konami code to unlock the lego api', () => {
     expect(true).toBe(true);
   });
 });
