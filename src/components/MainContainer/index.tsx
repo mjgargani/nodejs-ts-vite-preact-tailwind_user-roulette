6d534335972a16fc155bf5cbd90eb13b5f81ad30
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-04-12 15:07:41 -0300

    feat(components): mainContainer

---
+++ src/components/MainContainer/index.tsx
@@ -0,0 +1,6 @@
+import type Preact from 'preact';
+import './styles.css';
+
+const MainContainer: Preact.FunctionComponent = ({ children }) => <div id="main-container">{children}</div>;
+
+export default MainContainer;
